3607736eb9593d7c71d89543127367c0b73fb27b
Xander Kaylan <xxk3990@rit.edu>
2024-01-21 13:54:51 -0500

    added s3 url to cors on backend

--- server/app.js
+++ server/app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const cookieParser = require("cookie-parser")
 const cors = require('cors');
-const { expressjwt: jwt } = require("express-jwt");
+const {
+    expressjwt: jwt
+} = require("express-jwt");
 const process = require('process')
 const router = require("./router");
 
 
 const app = express();
 app.use(cookieParser());
-app.use(cors({ origin: "*"}))
+app.use(cors({
+    origin: "http://localhost:3001"
+}, {
+    origin: "https://www.astimemories.com"
+}))
 app.use(express.json());
 router(app);
 
 module.exports = app;
-
