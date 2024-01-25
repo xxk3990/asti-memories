95da718799227078b0bef81a6e1c83f8aa95fe99
Xander Kaylan <xxk3990@rit.edu>
2024-01-25 13:35:17 -0500

    finished homepage!

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -16,16 +16,16 @@ export default function Home() {
     return (
         <div className='Home'>
             <section className = "restaurant-info">
-            <h3 className='asti-verbiage'>
+            <p className='asti-verbiage'>
                     The <em>Asti</em>, a landmark New York City restaurant, brought opera from the uptown stage to
-                    the downtown dinner table from 1924 to 2000. Located in Greenwich Village, back in
-                    the day when “the Village” was the creative heart of Manhattan, the Asti was known as
+                    the downtown dinner table from 1924 to 2000. Located in Greenwich Village back in
+                    the days when the Village was the creative heart of Manhattan, the <em>Asti</em> was known as
                     the home of the singing waiters and the go-to hotspot to discover budding opera talents
                     who then went on to illustrious careers. Founder Adolfo Mariani, a baritone himself,
-                    created the Asti with the idea that opera was not only for the elite. He was convinced,
+                    created the <em>Asti</em> with the idea that opera was not only for the elite. He was convinced,
                     that served up with dinner, famous arias, duets, rousing choruses and a dose of
                     audience participation anyone could and would become an opera lover. And they did.
-                </h3>
+                </p>
                 <section className='asti-images'>
                     <img className='asti-img-big' src={astiFront} alt="asti-front"/>
                     <section className='asti-images-small'>
@@ -36,17 +36,17 @@ export default function Home() {
                 </section>
                 
                    
-                <h3 className='asti-verbiage'>
+                <p className='asti-verbiage'>
                     This is Angela Mariani, Adolfo’s youngest daughter, and my life’s goal has been to write
-                    a memoir about the Asti. 25 years have passed since the Asti closed, memories fade,
-                    including my own, and by sharing, you will help bring the Asti story to life.
+                    a memoir about the <em>Asti</em>. 25 years have passed since the <em>Asti</em> closed, memories fade,
+                    including my own, and by sharing, you will help bring the <em>Asti</em> story to life.
                     Whatever you choose to share will remain anonymous and you are not required to
                     provide any personal information.
-                </h3>
+                </p>
             </section>
             <section className='nav-btns'>
-                <button className='nav-btn' onClick={navigateToForm}>Go to Form</button>
-                <button className='nav-btn' onClick={navigateToPosts}>Go to Posts</button>
+                <button className='nav-btn' onClick={navigateToForm}>Share Memory</button>
+                <button className='nav-btn' onClick={navigateToPosts}>View Posts</button>
             </section>
         </div>
     )
