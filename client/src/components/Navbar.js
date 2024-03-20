878368efa441ca9713aad06ddcd943b36df3a6a2
Xander Kaylan <xxk3990@rit.edu>
2024-03-20 15:27:50 -0400

    fixed mobile friendliness on navbar and gallery

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -22,8 +22,9 @@ export default function Navbar() {
         return (
             <div className='Navbar'>
                 <section className='nav'>
-                    <span className='nav-item-home'><Link to='/'><em>Asti</em> Memories</Link></span>
+                    
                     <ul className = "nav-links">
+                        <li className='nav-item'><Link to='/'>Home</Link></li>
                         <li className='nav-item'><Link to='/memoryForm'>Form</Link></li>
                         <li className='nav-item'><Link to='/memories'>Memories</Link></li>
                         <li className='nav-item'><Link to='/gallery'>Gallery</Link></li>
